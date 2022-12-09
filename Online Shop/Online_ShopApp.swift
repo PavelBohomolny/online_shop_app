c417ece14fca8312562ce7c1f954997e27d86f71
NexusUA <pbogomolny93@gmal.com>
2022-12-09 15:36:56 +0100

    Initial Commit

---
+++ Online Shop/Online_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShopApp.swift
+//  Online Shop
+//
+//  Created by Pavel Bohomolnyi on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
